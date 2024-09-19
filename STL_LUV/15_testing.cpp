08e45a8a33e91c5c3b821f016e23327a5a7b2a0d
HIMANSHU KANSAL <himanshukansal101@gmail.com>
2024-09-19 21:36:53 +0530

    :)

--- STL_LUV/15_testing.cpp
+++ STL_LUV/15_testing.cpp
@@ -1,7 +1,11 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
 
-    cout<<"all code are run succcessfull......";
-    return 0;
+   pair<string, int>pr;
+    pr= {"himanshukansal",22132};//making a pair
+   cout<<pr.first<<"  "<<pr.second<<endl;
+   //map , multimap, set multiset
+
+    
 }
