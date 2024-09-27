98888ac8b0d916d74c889a500a4965d6c2b0f8fa
HIMANSHU KANSAL <himanshukansal101@gmail.com>
2024-09-27 21:52:56 +0530

    basic problems on recursion by striver

--- Recursions/3_Basic_problems_on_recursion.cpp
+++ Recursions/3_Basic_problems_on_recursion.cpp
@@ -1,14 +1,73 @@
-//printing name 4 times by using recursion
+// printing name 4 times by using recursion/////////////////////////////////////////
+                // #include<iostream>
+                // using namespace std;
+                // int n = 1;
+                // void printname(){
+                //     if(n>4)return;
+                //     cout<<n<<"    =>    himanshukansal 22132"<<endl;
+                //     n++;
+                //     printname();
+                // }
+                // int main(){
+                //         printname();
+                //         return 0 ;
+                // }
+
+                            //print linearly from 1 to N//////////////////////////////////////////////
+                                            // #include<iostream>
+                                            // using namespace std;
+                                          
+                                            // void printnumber(int i,int &n){
+                                            //     if(i>n)return;
+                                            //     cout<<i<<" ";
+                                            //     printnumber(i+1,n);
+                                            //     }
+                                            // int main(){
+                                            //     int n;
+                                            //     cin>>n;
+                                            //     printnumber(1,n);
+                                            //     return 0;
+                                           // }
+//print linearly from Nto 1//////////////////////////////////////////////
+// #include<iostream>
+// using namespace std;
+
+// void printnumber(int i,int &n){
+// if(i==0)return;
+// cout<<i<<" ";
+// printnumber(i-1,n);
+// }
+// int main(){
+// int n;
+// cin>>n;
+// printnumber(n,n);
+// return 0;
+// }                                          
+                        // print linearly from N to 1 but by backtracking//////////////////////////////////////////////
+                        // #include<iostream>
+                        // using namespace std;
+                        // void printnumber(int i,int &n){
+                        // if(i>n)return;
+                        // printnumber(i+1,n);
+                        //  cout<<i<<" ";
+                        // }
+                        // int main(){
+                        // int n;
+                        // cin>>n;
+                        // printnumber(1,n);
+                        // return 0;
+                        // }                  
+ // print linearly from 1 to N but by backtracking//////////////////////////////////////////////
 #include<iostream>
 using namespace std;
-int n = 1;
-void printname(){
-    if(n>4)return;
-    cout<<n<<"    =>    himanshukansal 22132"<<endl;
-    n++;
-    printname();
+void printnumber(int i,int &n){
+if(i==0)return;
+printnumber(i-1,n);
+    cout<<i<<" ";
 }
 int main(){
-        printname();
-        return 0 ;
-}
+int n;
+cin>>n;
+printnumber(n,n);
+return 0;
+}        
