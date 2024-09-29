2edfcf103d80d66f2f8ef26359f4931f1d9b6558
HIMANSHU KANSAL <himanshukansal101@gmail.com>
2024-09-29 23:39:22 +0530

    fibonacci series

---
+++ Recursions/5_c_fibonacci_series.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int fibo(int n){
+    if(n<=1){
+        return n;
+    }
+    int last  = fibo(n-1);
+    int s_last = fibo(n-2);
+    return last+s_last;
+}
+int main(){
+    int t;
+    cout<<"enter no of test cases: ";
+    cin>>t;
+    int k=0;
+    while(k<=t){
+    cout<<fibo(k) <<" ";
+    k++;
+    }
+    return 0;
+}
