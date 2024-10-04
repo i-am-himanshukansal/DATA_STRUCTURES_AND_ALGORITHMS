ad9324d7d08de0868a4e75df79f9358fc4460982
HIMANSHU KANSAL <himanshukansal101@gmail.com>
2024-10-04 15:04:47 +0530

    doubly 1,2

---
+++ Linked_list/doubly linked list/1_doubly.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node*next;
+    Node*prev;
+
+    Node(int data1){
+        this->data=data1;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+};
+Node*array_to_doubly_linkedlist(vector<int>v){
+        Node*head = new Node(v[0]);
+        Node*tail = head;
+        for(int i=1;i<v.size();i++){
+            Node*temp = new Node(v[i]);
+            tail->next = temp;
+            temp->prev = tail;
+            tail=temp;
+        }
+        return head;
+}
+void print_ll(Node*head){
+    Node*temp=head;
+    while(temp!=NULL){
+    cout<<temp->data<<" ";
+    temp=temp->next;
+    }
+    cout<<endl;
+}
+int main(){
+     vector<int>v = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        Node*head = array_to_doubly_linkedlist(v);
+        print_ll(head);
+        // Node*sample = new Node(45);
+        // sample->next = head;
+        // head->prev = sample;
+        // head = sample;
+
+        print_ll(head);
+
+        return 0;
+}
---
+++ Linked_list/doubly linked list/2_delete_head_doubly-ll.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node*next;
+    Node*prev;
+
+    Node(int data1){
+        this->data=data1;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+};
+Node*array_to_doubly_linkedlist(vector<int>v){
+
+
+        if(v.size()==0){
+            return NULL;
+        }
+        Node*head = new Node(v[0]);
+        Node*tail = head;
+        for(int i=1;i<v.size();i++){
+            Node*temp = new Node(v[i]);
+            tail->next = temp;
+            temp->prev = tail;
+            tail=temp;
+        }
+        return head;
+}
+void print_ll(Node*head){
+    Node*temp=head;
+    while(temp!=NULL){
+    cout<<temp->data<<" ";
+    temp=temp->next;
+    }
+    cout<<endl;
+}
+
+Node*delete_head(Node*head){
+    if(head==NULL || head->next == nullptr){
+        cout<<"this linked list is empty or single elemented linked list : ";
+        return NULL;
+    }
+    Node*temp = head;
+    head= head->next;
+    head->prev=nullptr;
+    temp->next=nullptr;
+    delete temp;
+    return head;
+}
+int main(){
+     vector<int>v={1,23,45,56,89,7,45,56,53,22};
+        Node*head = array_to_doubly_linkedlist(v);
+        cout<<"linked list before delete head node"<<" : ";
+        print_ll(head);
+
+        head = delete_head(head);
+        cout<<"linked list after delete head node"<<" : ";
+        print_ll(head);
+        cout<<head->prev<<endl;//return the prev of the head of the linked list
+
+        return 0;
+}
