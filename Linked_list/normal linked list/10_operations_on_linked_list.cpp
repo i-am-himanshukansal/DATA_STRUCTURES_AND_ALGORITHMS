208c5688785bb4ae647180deb4137c83d6fa3b30
HIMANSHU KANSAL <himanshukansal101@gmail.com>
2024-10-16 23:47:07 +0530

    operations of linked list

---
+++ Linked_list/normal linked list/10_operations_on_linked_list.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"insertion on head of linked list"<<endl;
+    cout<<"insertion on tail of linked list"<<endl;
+    cout<<"insertion on x position between linked list"<<endl;
+    cout<<"insertion at middle of linked list"<<endl;
+    cout<<"insertion before head"<<endl;
+    cout<<"find length of linked list"<<endl;
+    cout<<"reversing the linked list"<<endl;
+    cout<<"implement node left right to construct tree by linked list"<<endl;
+
+    return 0;
+}
