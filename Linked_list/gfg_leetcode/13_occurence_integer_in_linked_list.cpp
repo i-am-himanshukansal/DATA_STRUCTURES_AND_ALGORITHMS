446485b7f47149294d1d38d9dd7c4f68da64e5ea
HIMANSHU KANSAL <himanshukansal101@gmail.com>
2024-10-26 20:49:36 +0530

    ;))))

--- Linked_list/gfg_leetcode/13_occurence_integer_in_linked_list.cpp
+++ Linked_list/gfg_leetcode/13_occurence_integer_in_linked_list.cpp
@@ -30,7 +30,6 @@ struct node
 class Solution {
   public:
    int count(struct Node* head, int key) {
-        // add your code here
         int cnt = 0;
         Node*temp = head;
         while(temp!=NULL){
