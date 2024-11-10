8369f51cdafa4d4dc15a0b77265649b0073f1acb
HIMANSHU KANSAL <himanshukansal101@gmail.com>
2024-11-10 14:43:56 +0530

    detect_loop

---
+++ Linked_list/CIRCULAR_LINEKD_LIST/12a_detect_loop_in_linked_list.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+
+class node{
+       public:
+        int data;
+        node*next;
+
+        
+        node(int d){
+                this->data = d;
+                this->next =  NULL;
+        }
+};
+void print(node*head){
+        node*temp = head;
+
+
+        do{
+                cout<<temp->data<<" ";
+                temp= temp->next;
+        }while(temp!=head);
+        cout<<endl;
+}
+bool detect_loop(node*head){
+        node*temp = head;
+        map<node*,int>mpp;
+        if(head==NULL)return false;
+        while(temp!=NULL){
+                
+                if(mpp.find(temp)!=mpp.end()){
+                       return true;
+                }else{
+                         mpp[temp] = 1;
+                }
+
+                temp = temp->next;
+        }
+        return false;
+}
+
+node* createlinkedlist(vector<int>&v){
+        if(v.size()==0){
+                return NULL;
+        }else{
+                node*head = new node(v[0]);
+                node*tail = head;
+                tail->next = head;
+                for(int i=1;i<v.size();i++){
+                        node*temp = new node(v[i]);
+                        tail->next = temp;
+                        tail = temp;
+                        tail->next = head;
+                }
+                 return head;
+        }
+       
+}
+int main(){
+
+       
+      int t;
+      cout<<"enter test cases : ";
+      cin>>t;
+      while(t--){
+          int n;
+        cout<<"enter n = ";
+        cin>>n;
+
+        vector<int>v;
+        cout<<"enter elements for linked list : ";
+        for(int i=0;i<n;i++){
+                int x;
+                cin>>x;
+                v.push_back(x);
+        }
+
+        node*head = createlinkedlist(v);
+
+        print(head);
+        
+        
+        cout<<detect_loop(head)<<endl;
+        
+        
+      }
+        return 0;
+
+
+
+}
