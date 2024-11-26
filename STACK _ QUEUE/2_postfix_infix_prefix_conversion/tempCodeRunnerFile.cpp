5c6ad30ff7e32b934c11e8885717d74fa6f8e506
HIMANSHU KANSAL <himanshukansal101@gmail.com>
2024-11-26 10:18:14 +0530

    PREFIX TO INFIX, POSTFIX

--- STACK _ QUEUE/2_postfix_infix_prefix_conversion/tempCodeRunnerFile.cpp
+++ STACK _ QUEUE/2_postfix_infix_prefix_conversion/tempCodeRunnerFile.cpp
@@ -1,2 +1,13 @@
-
-        }else{
+  if(s[i]>='a'&&s[i]<='z'||s[i]>='A'&&s[i]<='Z'||s[i]>='0'&&s[i]<='9'){
+                        string str = "";
+                        str+=s[i];
+                        stri.push(str);
+                }else if(s[i]=='+'||s[i]=='-'||s[i]=='*'||s[i]=='/'||s[i]=='^'){
+                        string s1 = stri.top();
+                        stri.pop();
+                        string s2 = stri.top();
+                        stri.pop();
+                        string str = '('+s2+s[i]+s1+')';
+                        stri.push(str);
+                }
+                i++;
