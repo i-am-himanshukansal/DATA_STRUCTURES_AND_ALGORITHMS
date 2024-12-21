a7970d5582a122d43a4207717ff48719d77624ed
HIMANSHU KANSAL <himanshukansal101@gmail.com>
2024-12-21 18:03:43 +0530

    rotated

---
+++ Linked_list/gfg_leetcode/54_rotated_by_90_degree.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to rotate matrix anticlockwise by 90 degrees.
+    void rotateby90(vector<vector<int>>& mat) {
+        // code here
+        int n = mat[0].size();
+        
+        vector<vector<int>>v;
+        for(int i=n-1;i>=0;i--){
+            vector<int>level;
+            for(int j=0;j<n;j++){
+                level.push_back(mat[j][i]);
+            }
+            v.push_back(level);
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                mat[i][j] = v[i][j];
+            }
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int> > matrix(n);
+
+        for (int i = 0; i < n; i++) {
+            matrix[i].assign(n, 0);
+            for (int j = 0; j < n; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        ob.rotateby90(matrix);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j)
+                cout << matrix[i][j] << " ";
+            cout << endl;
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
